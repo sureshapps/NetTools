07ee1041f24b4f1c3cfd4118b256ca705d3aba28
Suresh Kaleyannan <hello@suresh.app>
2025-08-22 15:48:50 +0800

    sureshks

--- app/(navigation)/(code)/util/download.ts
+++
@@ -1,8 +0,0 @@
-const download = (dataURL: string, filename: string) => {
-  const link = document.createElement("a");
-  link.download = filename;
-  link.href = dataURL;
-  link.click();
-};
-
-export default download;
