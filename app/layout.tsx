efcd2ea478e817df32fa3c14a1d0eb71b8b1322f
Suresh Kaleyannan <hello@suresh.app>
2025-08-22 11:25:05 +0800

    /(navigation)/

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,65 @@
-import { Analytics } from "@vercel/analytics/react";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Fira_Code, IBM_Plex_Mono, JetBrains_Mono, Roboto_Mono } from "next/font/google";
 import cn from "classnames";
-import { BASE_URL } from "@/utils/common";
-import { TooltipProvider } from "@/components/tooltip";
-import { Viewport } from "next";
-import { Log } from "./log";
-import { Toaster } from "@/components/toast";
-import { Suspense } from "react";
+import { Navigation } from "@/components/navigation";
+import { GeistMono } from "geist/font/mono";
+import localFont from "next/font/local";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"], weight: ["400", "500"], display: "swap" });
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: "500",
+  display: "swap",
+  variable: "--font-jetbrainsmono",
+});
+const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: "500",
+  display: "swap",
+  variable: "--font-ibmplexmono",
+});
+const firaCode = Fira_Code({ subsets: ["latin"], weight: "400", display: "swap", variable: "--font-firacode" });
+const soehneMono = localFont({
+  src: "../assets/soehne-mono-buch.woff2",
+  variable: "--font-soehne-mono",
+});
+const commitMono = localFont({
+  src: "../assets/commit-mono-regular.woff2",
+  variable: "--font-commitmono",
+});
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
 
-const title = "Ray.so";
-const description = "Ray.so";
+/**
+ * We can't adjust the fallback stack of the font so instead we just extract the
+ * font name and create our own CSS variable using it so that we can configure the
+ * font stack to include emoji fonts.
+ */
+const geistMonoFontName = GeistMono.style.fontFamily.split(",")[0];
 
-export const metadata = {
-  metadataBase: new URL(BASE_URL),
-  title: title,
-  description: description,
-  openGraph: {
-    type: "website",
-    siteName: "Ray.so",
-  },
-  twitter: {
-    card: "summary_large_image",
-    creator: "@raycast",
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: "#181818",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function NavigationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
-      <TooltipProvider>
-        <body className={cn("isolate", inter.className)}>
-          <Log />
-
-          {children}
-          <Toaster position="top-center" offset={70} duration={2000} />
-        </body>
-      </TooltipProvider>
-      <Analytics />
-    </html>
+    <div
+      className={cn(
+        "h-full",
+        jetBrainsMono.variable,
+        ibmPlexMono.variable,
+        firaCode.variable,
+        soehneMono.variable,
+        commitMono.variable,
+        robotoMono.variable,
+      )}
+      style={
+        {
+          "--font-geist-mono": geistMonoFontName,
+        } as React.CSSProperties
+      }
+    >
+      <Navigation />
+      <main className="flex flex-col min-h-full pt-[50px]">{children}</main>
+    </div>
   );
 }
